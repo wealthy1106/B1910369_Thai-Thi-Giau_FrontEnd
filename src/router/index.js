1a3330a53708a68670d48a8fa9ce0ecb16e7158f
wealthy <thaigiauvlvn@gmail.com>
2023-02-21 23:00:11 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,12 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+
+    },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
