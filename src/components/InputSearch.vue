3729a783166bb1580aac7846c694fa4205036601
wealthy1106 <113654917+wealthy1106@users.noreply.github.com>
2023-03-28 11:39:56 +0700

    update icon

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -20,7 +20,7 @@ export default {
             @input="updateModelValue" @keyup.enter="submit" />
         <div class="input-group-append">
             <button class="btn btn-outline-secondary" type="button" @click="submit">
-                <i class="fas fa-search"></i> Tìm kiếm
+                <img src="../assets/search.PNG" style="width: 22px;"> Tìm kiếm
             </button>
         </div>
     </div>
